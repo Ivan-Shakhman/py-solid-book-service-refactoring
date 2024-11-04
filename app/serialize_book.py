d8c6e3c57ae09581fe6a01516235219a65220d31
Ivan Shakhman <shakhmanivan@gmail.com>
2024-11-04 11:44:50 +0200

    now ready to merge!

--- app/serialize_book.py
+++ app/serialize_book.py
@@ -5,14 +5,18 @@ from xml.etree import ElementTree
 
 class BaseSerializeBook(ABC):
 
+    def __init__(self, title: str, content: str) -> None:
+        self.title = title
+        self.content = content
+
     @abstractmethod
-    def serialize(self, method_type: str) -> None:
+    def serialize(self, method_type: str) -> str:
         pass
 
 
 class XmlSerializeBook(BaseSerializeBook):
 
-    def serialize(self, method_type: str) -> ElementTree.tostring:
+    def serialize(self, method_type: str) -> str:
         if method_type == "xml":
             root = ElementTree.Element("book")
             title = ElementTree.SubElement(root, "title")
@@ -20,9 +24,8 @@ class XmlSerializeBook(BaseSerializeBook):
             content = ElementTree.SubElement(root, "content")
             content.text = self.content
             return ElementTree.tostring(root, encoding="unicode")
-
         else:
-            super().serialize(method_type)
+            return super().serialize(method_type)
 
 
 class JsonSerializeBook(BaseSerializeBook):
@@ -31,8 +34,8 @@ class JsonSerializeBook(BaseSerializeBook):
         if method_type == "json":
             return json.dumps({"title": self.title, "content": self.content})
         else:
-            super().serialize(method_type)
+            return super().serialize(method_type)
 
 
-class SerializeBook(JsonSerializeBook, XmlSerializeBook):
+class SerializeBook(XmlSerializeBook, JsonSerializeBook):
     pass
